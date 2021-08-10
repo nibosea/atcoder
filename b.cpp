fdd36f7e616ca7aa28ad361840c49154aedd4589
kinjo <kinchannibo@yahoo.co.jp>
2021-08-10 13:50:10 +0900

    neko

--- b.cpp
+++ b.cpp
@@ -26,31 +26,4 @@ const ll INF = 1'000'000'000'000'000'000;
 //Write From this Line
 int main()
 {
-	cin.tie(0);
-	ios_base::sync_with_stdio(false);
-<<<<<<< HEAD
-	int n;
-	cin >> n;
-	vector<ll> a(n), b(n);
-	rep(i,n) cin >> a[i];
-	rep(i,n) cin >> b[i];
-=======
-	string x;
-	cin >> x;
-	bool flag = true;
-	rep(i,3){
-		if((x[i]-'0' + 1)%10 == x[i+1]-'0'){
-		} else {
-			flag = false;
-		}
-	}
-	x.erase(unique(x.begin(),x.end()),x.end());
-	debug(x);
-	if(x.size() == 1 || flag){
-		puts("Weak");
-		return 0;
-	} else {
-		puts("Strong");
-	}
->>>>>>> c6ec2ec2c6a3af6a417a3f688a484e837f234d1e
 }

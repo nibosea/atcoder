945f30be1ee4891803c2081c5b0e6d49675e709c
kinjo <kinchannibo@yahoo.co.jp>
2021-08-25 17:18:43 +0900

    blog 25.md

--- a.cpp
+++ a.cpp
@@ -24,37 +24,52 @@ const ll MOD = 998244353;
 const double PI=3.14159265358979323846;
 const int inf = 1001001001;
 const ll INF = 1'000'000'000'000'000'000;
-//Write From this Lijjjjjjjjjne
+int n;
+vector<tuple<int,int,int>> t; // weight v1, v2
+struct UnionFind {
+	vector<int> d;
+	UnionFind(int n=0): d(n,-1) {} //このコロンってなんですか
+	int find(int x){
+		if (d[x] < 0) return x; //x:根
+		return d[x] = find(d[x]);//x:子 -> xの親の根を返す
+	}
+	bool unite(int x, int y) {
+		x = find(x); y = find(y);//根同士をくっつける。
+		if ( x ==  y) return false ; 
+		/*連結成分が一致してるんだったらfalse返す。
+			minimum spanning tree (クラスカル法と言うのをやるときに、くっつけたかどうかを取れると嬉しい.
+			chmin/chmaxみたいなノリかな
+		 */
+		if ( d[x] > d[y]) swap(x,y);//xの方が大きいようにする（ただしサイズは負デモっているので不等号が逆）
+		d[x] += d[y] ; //xにyをくっつける.
+		d[y] = x;
+		return true; 
+	}
+	bool same(int x, int y) { return find(x) == find(y) ; }
+	int size(int x) { return -d[find(x)];}
+};
+
+void input(){
+    cin >> n;
+    t.resize(n-1);
+    rep(i,n-1){
+        int v1, v2, w;
+        cin >> v1 >> v2 >> w;
+        v1--, v2--;
+        t[i] = make_tuple(w,v1,v2);
+    }
+    SORT(t);
+}
 int main()
 {
-    int n, m; cin >> n >> m;
-    vector<int> a(n);
-    rep(i,n) cin >> a[i];
-    vector<int> b(m);
-    rep(i,m) cin >> b[i];
-    int dist = inf;
+    input();
+    UnionFind uf(n);
+    ll ans = 0;
     rep(i,n-1){
-        if(a[1+i] != a[0] || a[n-1-i] != a[0]){
-            dist = i+1;
-            break;
-        }
-    }
-    int ans = 0;
-    bool flag = false; // distの移動をしたかどうか
-    int now = a[0];
-    rep(i,m){
-        if(now != b[i]){
-            if(flag) ans++;
-            else if(dist == inf){
-                cout << -1 << endl;
-                return 0;
-            } else {
-                ans += dist;
-                flag = true;
-            }
-        }
-        now = b[i];
-        ans++; // 実際に0,1を取る操作
+        ll v1, v2, w;
+        tie(w,v1,v2) = t[i];
+        ans += w * uf.size(v1) * uf.size(v2);
+        uf.unite(v1,v2);
     }
     cout << ans << endl;
 }
--- b.cpp
+++ b.cpp
@@ -20,73 +20,41 @@ void rswap(ll &a, ll &b){ if(a <= b) swap(a,b); }
 const ll MOD = 998244353;
 //Write From this Line
 ll n;
-vector<vector<int>> s;
-vector<int> p;
+vector<P> p;
 void input(){
     cin >> n;
-    s.resize(4,vector<int>(4));
     p.resize(n);
-    rep(i,4)rep(j,4) cin >> s[i][j];
-    rep(i,n) cin >> p[i];
+    rep(i,n){
+        cin >> p[i].first >> p[i].second;
+    }
+    SORT(p);
+    //rep(i,n){
+    //    printf("(%lld, %lld)\n", p[i].first, p[i].second);
+    //}
 }
 int main()
 {
     input();
-    rep(k,n){
-        rep(i,4)rep(j,4) if(p[k] == s[i][j]) s[i][j] = 0;
+    ll ok = 0, ng = 1e9;
+    while(ng - ok > 1){
+        ll k =(ok + ng) / 2;
+        queue<P> q;
+        ll M = -1;
+        ll m = 1e9+5;
+        bool flag = false;
+        rep(i,n){
+            while(!q.empty() && p[i].first - q.front().first >= k){
+                ll x, y;
+                tie(x,y) = q.front(); q.pop();
+                chmax(M,y);
+                chmin(m,y);
+            }
+            if( M-p[i].second >= k) flag = true;
+            if(p[i].second-m >= k) flag = true;
+            q.push(p[i]);
+        }
+        if(flag) ok = k;
+        else ng = k;
     }
-    //rep(i,4){
-    //    rep(j,4){
-    //        cout << s[i][j] << " ";
-    //    }
-    //    cout << endl;
-    //}
-    int ans = 0;
-    vector<int> num(0);
-    rep(i,4){
-        int cnt = 0;
-        int sum = 0;
-        rep(j,4){
-            sum += s[i][j];
-            if(s[i][j] != 0) cnt++;
-        }
-        // cnt = 1の場合がリーチ
-        if(cnt == 1){
-            num.push_back(sum);
-            ans++;
-        }
-    }
-    rep(j,4){
-        int cnt = 0;
-        int sum = 0;
-        rep(i,4){
-            sum += s[i][j];
-            if(s[i][j] != 0) cnt++;
-        }
-        // cnt = 1の場合がリーチ
-        if(cnt == 1){
-            num.push_back(sum);
-            ans++;
-        }
-    }
-    // 縦横に関してはこれでok
-    // 正斜め
-    int cnt = 0;
-    int sum = 0;
-    rep(k,2){
-        rep(i, 4) {
-            sum += s[i][i];
-            if (s[i][i] != 0) cnt++;
-        }
-        if(cnt == 1){
-            ans++;
-            num.push_back(sum);
-        }
-        rep(j,4){
-            REV(s[j]);
-        }
-    }
-    SORT(num);
-    if(ans) cout << num[0] << endl;
-    else cout << "no" << endl;
+    cout << ok << endl;
 }

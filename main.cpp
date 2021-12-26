1aaa31313f042326922e5f7ccf2dcd39263de7fd
kinjo <kinchannibo@yahoo.co.jp>
2021-12-26 23:32:01 +0900

    俺が天才ってワケ

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll =unsigned long long;
-typedef pair<ll,ll> P;
+using ll = long long;
+typedef pair<int,int> P;
 #define SORT(a) sort((a).begin(),(a).end())
 #define REV(a) reverse((a).begin(),(a).end())
 #define For(i, a, b)    for(int i = (a) ; i < (b) ; ++i)
 #define rep(i, n)       For(i, 0, n)
+#define Per(i, a, b)    for(int i = (a) ; i>=(b);--i)
+#define per(i, n)       Per(i,0,n)
 #define debug(x)  cerr << #x << " = " << (x) << endl;
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }

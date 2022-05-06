809ccf4731da7db46e2a8696fb61c450da141cd4
nibosea <kinchannibo@yahoo.co.jp>
2022-05-06 20:03:15 +0900

    いつもの活動

--- contest.sh
+++ contest.sh
@@ -2,13 +2,17 @@
 #source contest.sh abcXXXのほうがいいよ
 ARRAY=(a.cpp b.cpp c.cpp d.cpp e.cpp f.cpp g.cpp h.cpp)
 
-mkdir problems/$1
-cp go.sh problems/$1/
-for i in ${ARRAY[@]}
-do
-	#mv $i bef_$i
+if [ -e ./problems/$1 ]; then
+	echo exists
+else 
+	mkdir problems/$1
+	cp go.sh problems/$1/
+	for i in ${ARRAY[@]}
+	do
+#mv $i bef_$i
 	touch problems/$1/$i
 	cp main.cpp problems/$1/$i
-done
+	done
+fi
 cd problems/$1
 chmod +x go.sh
--- problems/arc121/d.cpp
+++ problems/arc121/d.cpp
@@ -2,14 +2,13 @@
 #include <atcoder/all>
 
 using namespace std;
+using namespace atcoder;
 using ll = long long;
 typedef pair<int,int> P;
 #define SORT(a) sort((a).begin(),(a).end())
 #define REV(a) reverse((a).begin(),(a).end())
 #define For(i, a, b)    for(int i = (a) ; i < (b) ; ++i)
 #define rep(i, n)       For(i, 0, n)
-#define Per(i, a, b)    for(int i = (a) ; i>=(b);--i)
-#define per(i, n)       Per(i,n,0)
 #define debug(x)  cerr << #x << " = " << (x) << endl;
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
@@ -29,30 +28,9 @@ const ll MOD = 998244353;
 const double PI=3.14159265358979323846;
 const int inf = 1001001001;
 const ll INF = 1'000'000'000'000'000'000;
+vector<int> to[200'005];
 //Write From this Line
 int main()
 {
-	int n;
-	cin >> n;
-	string s; cin >> s; string t; cin >> t;
-	if(count(s.begin(),s.end(),'0') != count(t.begin(),t.end(),'0')){
-		cout << -1 << endl;
-		return 0;
-	}
-	if(s==t){
-		cout << 0 << endl;
-		return 0;
-	}
-	int ans = 0;
-	int sima = 0;
-	vector<int> a(0);
-	vector<int> b(0);
-	rep(i,n){
-		if(s[i] == '0') a.push_back(i);
-		if(t[i] == '0') b.push_back(i);
-	}
-	rep(i,a.size()){
-		if(a[i] != b[i]) ans++;
-	}
-	cout << ans << endl;
+
 }

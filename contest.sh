39ca774a75c2540396bfa99bc74d7c50cae76f3a
nibosea <kinchannibo@yahoo.co.jp>
2022-05-06 22:23:12 +0900

    neko

--- contest.sh
+++ contest.sh
@@ -4,6 +4,8 @@ ARRAY=(a.cpp b.cpp c.cpp d.cpp e.cpp f.cpp g.cpp h.cpp)
 
 if [ -e ./problems/$1 ]; then
 	echo exists
+elif [ -e ./pastproblems/$1 ]; then
+	echo past_exists
 else 
 	mkdir problems/$1
 	cp go.sh problems/$1/
